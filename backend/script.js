a3b0b8f7c66822b7de0969c87ad5e08a08983db0
Arjun R Devadiga <arjunr.is23@sahyadri.edu.in>
2026-06-07 16:50:36 +0530

    updated code 6 debugs

--- backend/script.js
+++ backend/script.js
@@ -43,7 +43,11 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-
+app.get("/debug", (req, res) => {
+    res.json({
+        message: "debug route working"
+    });
+});
 app.listen(PORT, () => {
 
     console.log(`Server running on port ${PORT}`);
